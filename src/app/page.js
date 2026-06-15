c2c8a5b0d51dbd96963babb1f5a203b7dd5fbbc7
Lebo Madikane <madikanelebohang@gmail.com>
2026-06-15 12:55:31 +0200

    create homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div>
       <main>
-        <>Lebo Madikane</>
+        <h1>Lebo Madikane</h1>
+        <p>Web Developer and Content Creator</p>
+        <p>Let's Work Together</p>
       </main>
-    </div>
   );
 }
