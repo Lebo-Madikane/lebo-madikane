25fb8800ae95dd9ace972714421be4d56e2445f8
Lebo Madikane <madikanelebohang@gmail.com>
2026-06-18 20:16:41 +0200

    create component

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import styles from './About.module.scss';
+import Button from '../UI/Button/Button';
+
+
+export default function About() {
+
+    return (
+        <section className={styles.about}>
+            <div className={styles.container}>
+
+            </div>
+        </section>
+    )
+}
