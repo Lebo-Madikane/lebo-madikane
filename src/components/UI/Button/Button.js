e817708850c6d58ff82056c244526647dbe6500f
Lebo Madikane <madikanelebohang@gmail.com>
2026-06-18 20:18:18 +0200

    create and style component

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import styles from './Button.module.scss';
+
+export default function Button({
+    children,
+    variant = 'primary',
+    size = 'md',
+    href,
+    type = 'button',
+    onClick,
+
+}) {
+
+    const classNames = ` ${styles.button} ${styles[variant]} ${styles[size]}`;
+
+    if (href) {
+        return (
+            <Link href={href} className={classNames}>{children}</Link>
+        )
+    };
+
+    return (
+        <button type={type} onClick={onClick} className={classNames}>
+            {children}
+        </button>
+    );
+
+}
