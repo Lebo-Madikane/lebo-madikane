fe2c8d6b67d2844e9a9c4a3509c7b0a28c3f3a4a
Lebo Madikane <madikanelebohang@gmail.com>
2026-06-22 21:19:09 +0200

    add Projects component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header/Header";
 import Hero from "@/components/Hero/Hero";
 import About from "@/components/About/About";
+import Projects from "@/components/Projects/Projects";
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <Header />
       <Hero />
       <About />
+      <Projects />
     </main>
   );
 
