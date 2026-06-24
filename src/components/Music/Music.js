6c2c18a771727ec831211c1967c4cec10b70d1bf
Lebo Madikane <madikanelebohang@gmail.com>
2026-06-24 19:58:43 +0200

    create and style component

---
+++ src/components/Music/Music.js
@@ -0,0 +1,12 @@
+import styles from './Music.module.scss';
+
+export default function Music() {
+
+    return (
+        <section className={styles.music}>
+            <div className={styles.container}>
+
+            </div>
+        </section>
+    );
+}
