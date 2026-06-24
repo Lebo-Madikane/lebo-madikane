f71f27137df2f7c2e66f942f4606b4b7016f5d83
Lebo Madikane <madikanelebohang@gmail.com>
2026-06-24 20:03:10 +0200

    update and add component

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@ import Header from "@/components/Header/Header";
 import Hero from "@/components/Hero/Hero";
 import About from "@/components/About/About";
 import Projects from "@/components/Projects/Projects";
+import Music from "@/components/Music/Music";
+import Contact from "@/components/Contact/Contact";
+import Footer from "@/components/Footer/Footer";
 
 export default function HomePage() {
   return (
@@ -10,6 +13,9 @@ export default function HomePage() {
       <Hero />
       <About />
       <Projects />
+      <Music />
+      <Contact />
+      <Footer />
     </main>
   );
 
