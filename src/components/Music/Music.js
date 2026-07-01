2aaf6b18b75eab2abf5ccae0a930ec868915ff65
Lebo Madikane <madikanelebohang@gmail.com>
2026-07-01 13:05:35 +0200

    structure component content

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -3,9 +3,17 @@ import styles from './Music.module.scss';
 export default function Music() {
 
     return (
-        <section className={styles.music}>
+        <section className={styles.musicSection}>
             <div className={styles.container}>
-
+                <span className={styles.statementRight}>Curated Deep & Afro-house Music</span>
+                <div className={styles.content}>
+                    <div className={styles.contentContainer}>
+                        <div className={styles.header}></div>
+                        <div className={styles.images}></div>
+                        <div className={styles.cta}></div>
+                    </div>
+                </div>
+                <span className={styles.statementLeft}>Curated Deep & Afro-house Music</span>
             </div>
         </section>
     );
