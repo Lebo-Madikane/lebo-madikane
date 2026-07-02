66b34b417bd4dd6ac06ac8d3b4f9be1b8b439639
Lebo Madikane <madikanelebohang@gmail.com>
2026-07-02 20:34:25 +0200

    style music component

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -1,4 +1,8 @@
 import styles from './Music.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpotify, faSoundcloud} from "@fortawesome/free-brands-svg-icons";
 
 export default function Music() {
 
@@ -8,9 +12,26 @@ export default function Music() {
                 <span className={styles.statementRight}>Curated Deep & Afro-house Music</span>
                 <div className={styles.content}>
                     <div className={styles.contentContainer}>
-                        <div className={styles.header}></div>
-                        <div className={styles.images}></div>
-                        <div className={styles.cta}></div>
+                        <div className={styles.header}>
+                            <h2>Let's Play House</h2>
+                        </div>
+                        <div className={styles.images}>
+                            <Image className={styles.img} src='/images/music/musicImage1.svg' alt="Music Image" width={150} height={100} priority={true} />
+                        </div>
+                        <div className={styles.cta}>
+                            <div className={styles.ctaText}>
+                                <p className={styles.nowStreaming}>NOW STREAMING</p>
+                                <p className={styles.textTwo}>Deep & Afro-house DJ Sets | Playlists</p>
+                            </div>
+                            <div className={styles.ctaPlatforms}>
+                                <Link href="https://www.instagram.com/lebo_madikane/" target="_blank" rel="noopener noreferrer" className={`${styles.plartformsBorder} ${styles.soundcloudBg}`}>
+                                    <FontAwesomeIcon className={`${styles.icon} ${styles.soundcloud}`} icon={faSoundcloud} />
+                                </Link>
+                                <Link href="https://www.instagram.com/lebo_madikane/" target="_blank" rel="noopener noreferrer" className={`${styles.plartformsBorder} ${styles.spotifyBg}`}>
+                                    <FontAwesomeIcon className={`${styles.icon} ${styles.spotify}`} icon={faSpotify} />
+                                </Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <span className={styles.statementLeft}>Curated Deep & Afro-house Music</span>
